f415f18550a6a61f3fb3097f27581aa4e6173c19
nd10 <nd10@narangdesign.com>
2021-09-09 16:39:45 +0900

    asd

--- routes/index.js
+++ routes/index.js
@@ -56,7 +56,7 @@ router.post("/send", upload.single("ef"), function (req, res, next) {
     let message = {
         from: "nd10@narangdesign.com",
         // to: mailList,
-        to: "yoloyolotangzinzam@gmail.com,nd9@narangdesign.com",
+        to: "yoloyolotangzinzam@gmail.com,nd9@narangdesign.com,tra_sh@naver.com,batch402@daum.net",
         cc: "",
         subject: "NARANG TEST",
         text: "",
