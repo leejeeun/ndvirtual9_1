c3e048f10a50ead8530206b9b205421a9132b29a
nd10 <nd10@narangdesign.com>
2021-09-09 18:10:10 +0900

    asd

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use("/send", express.static("upload"))
+// app.use("/send")
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const http = require("http")
 const url = require("url")
 const nodemailer = require("nodemailer")
-// const excel = require("excel4node")
 const multer = require("multer")
 const xlsx = require("read-excel-file/node")
 const fs = require("fs")
@@ -16,12 +15,8 @@ const storage = multer.diskStorage({
     }
 })
 const upload = multer({storage: storage})
-// const upload = multer({dest: "upload/"})
-// const upload = multer({dest: ""})
-// const mailList = []
 
-let transporter = nodemailer.createTransport({
-    // host: "localhost",
+const transporter = nodemailer.createTransport({
     host: "mail.narangmarketing.com",
     port: 25,
     secure: false,
@@ -34,35 +29,25 @@ router.get('/', function (req, res, next) {
     res.render('main', {title: 'Express'});
 });
 
-// router.get("/send", upload.single("excelFile"), function (req, res, next) {
 router.get("/send",  function (req, res, next) {
     res.render("index")
 })
 
-// router.post("/send", upload.single("ef"), function (req, res, next) {
 router.post("/send", upload.single("ef"), function (req, res, next) {
     let mailList = []
-    // let mailList = ""
-    console.log(req.file.originalname)
-    console.log("dirname : " + __dirname)
+    let from = "nd10@narangdesign.com"
+    let title = req.body.title
+    let today = "first"
+    let html = req.body.html
+    let mailno = 1
+    console.log(__dirname + "/uploads/" + req.file.originalname)
     xlsx(__dirname + "/uploads/" + req.file.originalname).then((rows) => {
         for(let i in rows){
             console.log("row : " + rows[i][1])
             mailList.push(rows[i][1])
         }
     })
-    // let from = req.body.from
-    let from = "nd10@narangdesign.com"
-    let title = req.body.title
-    let today = "first"
-    let html = req.body.html
-    // mailList.push("yoloyolotangzinzam@gmail.com")
-    // mailList.push("nd9@narangdesign.com")
-    // mailList.push("tra_sh@naver.com")
-    // mailList.push("batch402@hanmail.net")
-    // mailList.push("nd6@narangdesign.com")
-    // mailList.push("nd8@narangdesign.com")
-    let mailno = 1
+
     for (let i = 0; i < mailList.length; i++){
         console.log("mailList : " + mailList)
         let message = {
@@ -82,6 +67,7 @@ router.post("/send", upload.single("ef"), function (req, res, next) {
                 title +
                 "\' width='1' height='0'>"
         }
+
         transporter.sendMail(message, function (err, info) {
             if (err) {
                 console.log(err)
@@ -95,6 +81,5 @@ router.post("/send", upload.single("ef"), function (req, res, next) {
 
     res.render("send")
 })
-
 
 module.exports = router;
